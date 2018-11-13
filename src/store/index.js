be09183e0783b7c970d73632481c36c83681f95c
Leng <lengzhang@live.com>
2018-11-13 15:44:39 -0800

    add redux-devtools-extension

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import { createLogger } from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 let middleware = [ thunk ];
 
 // 如果是在客户端环境，并且是开发模式，那么打印redux日志
 if (process.env.NODE_ENV == 'development' && __CLIENT__) middleware.push(createLogger());
 
+const composeEnhancers = process.env.NODE_ENV == 'development' && __CLIENT__ ? composeWithDevTools : compose;
+
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
-    compose(
+    composeEnhancers(
       applyMiddleware(...middleware),
     )
   )
